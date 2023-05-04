a340e06ccfaa70bbce33058c27593a81e4ec0c94
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-04 19:24:16 +0100

    fix:Añadido getNombre en la clase Cliente

--- src/com/hiperdino/valledeguerra/Cliente.java
+++ src/com/hiperdino/valledeguerra/Cliente.java
@@ -2,31 +2,35 @@ package com.hiperdino.valledeguerra;
 
 import java.util.Stack;
 
-    public class Cliente {
+public class Cliente {
 
-        private String nombre;
-        private Stack<String> cesta;
+    private String nombre;
+    private Stack<String> cesta;
 
-        public Cliente(String nombre, Stack<String> cesta) {
-            this.nombre = nombre;
-            this.cesta = cesta;
-        }
-
-        public Cliente(String nombre) {
-            this.nombre = nombre;
-            this.cesta = new Stack<>();
-        }
-
-        public void añadirProducto(String producto) {
-            cesta.add(producto);
-        }
-
-        public String quitarProducto() {
-            return cesta.pop();
-        }
-
-        public int getTotalProductosEnCesta() {
-            return cesta.size();
-        }
+    public Cliente(String nombre, Stack<String> cesta) {
+        this.nombre = nombre;
+        this.cesta = cesta;
     }
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        this.cesta = new Stack<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void añadirProducto(String producto) {
+        cesta.add(producto);
+    }
+
+    public String quitarProducto() {
+        return cesta.pop();
+    }
+
+    public int getTotalProductosEnCesta() {
+        return cesta.size();
+    }
+}
 
