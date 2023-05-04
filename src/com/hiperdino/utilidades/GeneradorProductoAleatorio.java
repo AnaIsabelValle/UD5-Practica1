a82603e3397a87a4866aadcd6d27f36f294c1a2a
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-04 14:51:05 +0100

    feat: Creación clase generador de productos aleatorios

---
+++ src/com/hiperdino/utilidades/GeneradorProductoAleatorio.java
@@ -0,0 +1,29 @@
+package com.hiperdino.utilidades;
+
+import java.util.Random;
+
+public class GeneradorProductoAleatorio {
+
+    private static String [] productos ={
+            "Papas",
+            "Huevos",
+            "Agua",
+            "Pollo",
+            "Pan",
+            "Queso",
+            "Mantequilla",
+            "Plátanos",
+            "Manzana",
+            "Tomate",
+            "Lechuga",
+            "Leche"
+    };
+
+    private static Random productoAleatorio = new Random();
+
+    public static String getRandomProductos(){
+        int index = productoAleatorio.nextInt(productos.length);
+        return productos[index];
+    }
+
+}
