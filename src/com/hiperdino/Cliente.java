9d2e70851dd0d6555e7007861911976498197a77
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-04 15:22:03 +0100

    feat: Creación clase cliente, sus atributos y método:añadir,quitar productos y total de productos en la cesta

---
+++ src/com/hiperdino/Cliente.java
@@ -0,0 +1,31 @@
+package com.hiperdino;
+
+import java.util.Stack;
+
+public class Cliente {
+
+    private String nombre;
+    private Stack<String> cesta;
+
+    public Cliente(String nombre, Stack<String> cesta) {
+        this.nombre = nombre;
+        this.cesta = cesta;
+    }
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        this.cesta = new Stack<>();
+    }
+
+    public void añadirProducto(String producto) {
+        cesta.add(producto);
+    }
+
+    public String quitarProducto() {
+        return cesta.pop();
+    }
+
+    public int getTotalProductosEnCesta() {
+        return cesta.size();
+    }
+}
